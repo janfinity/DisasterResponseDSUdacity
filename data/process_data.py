746fa7f14802a8e66406c85f74800bc8a90983ee
Jan Manneschmidt <jan.manneschmidt@icloud.com>
2021-12-05 11:39:12 +0100

    PROCESS DATA PIPELINE
    Based on the Jupyter Notebook the data processing pipeline has been
    coded and saved in the process_data.py file.
    
    Some minor changes to the Jupyter Notebook have been added.

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,88 @@
+
+#import all relevant libraries
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Function to load messages data as well as the categories data
+
+    Arguments:
+        messages_filepath : path leading to the messages csv-file
+        categories_filepath : path leading to the categories csv-file
+
+    Output:
+        df : combined data of messages and categories
+    """
+    #read messages csv
+    messages = pd.read_csv(messages_filepath)
+    #read categories csv
+    categories = pd.read_csv(categories_filepath)
+    #combine both dataframes in a new dataframe
+    df = pd.merge(messages, categories, on='id')
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Fuction to wrangle and clean the data in the dataframe
 
+    Arguments:
+        df : combined data of messages and categories
+
+    Output:
+        df : wrangled and cleaned dataframe of the combined data
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(pat=';', expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    # extract a list of new column names for categories
+    category_colnames = [r[:-2] for r in row]
+    #rename columns
+    categories.columns = category_colnames
+
+    #Convert values to numbers of 0 and 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1:]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df.drop(['categories'], axis=1, inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    #Drop column 'original' and 'child_alone'
+    df.drop(['original', 'child_alone'], axis=1, inplace=True)
+
+    #Drop rows containing other values than 0 or 1
+    df = df[df['related'] != 2]
+
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Function to save the data to a SQLite database
 
+    Arguments:
+        df -> Combined and cleaned data
+        database_filename -> path to SQLite database
+    """
+
+    engine = create_engine('sqlite:///'+ database_filename)
+    #create variabl to convert filepath into tablename
+    table_name = database_filename.replace(".db","")
+    df.to_sql(database_filename, engine, index=False, if_exists='replace')
 
 def main():
     if len(sys.argv) == 4:
@@ -24,12 +95,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
